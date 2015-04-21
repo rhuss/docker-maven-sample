fa5936b646b0c8166f98c0fe126f1bf370383463
Roland Huß <roland@jolokia.org>
2015-04-21 12:54:13 +0200

    Enhance for smarter docker host handling
    (still can be improved)

--- src/test/java/org/jolokia/docker/maven/sample/jolokia/LogServiceIT.java
+++ src/test/java/org/jolokia/docker/maven/sample/jolokia/LogServiceIT.java
@@ -1,11 +1,14 @@
 package org.jolokia.docker.maven.sample.jolokia;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.parsing.Parser;
 import org.junit.Test;
 
-import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static com.jayway.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsString;
 
 /**
  * @author roland
@@ -17,7 +20,8 @@ public class LogServiceIT {
     public void testLog() {
         long nonce = (int) (Math.random() * 10000);
 
-        RestAssured.baseURI = System.getProperty("log.url");
+        String url = System.getProperty("log.url");
+        RestAssured.baseURI = url != null ? url : fetchUrl();
         RestAssured.defaultParser = Parser.TEXT;
 
         given()
@@ -26,5 +30,17 @@ public class LogServiceIT {
         .then().assertThat()
                 .header("content-type", containsString("text/plain"))
                 .body(containsString(nonce + ""));
+    }
+
+    private String fetchUrl() {
+        String host = prepareHost(System.getProperty("log.host"));
+        String port = System.getProperty("log.port");
+        return "http://" + host + ":" + port;
+    }
+
+    private String prepareHost(String host) {
+        Pattern dockerHostPattern = Pattern.compile("^tcp://(.*?)(:\\d+)?$");
+        Matcher matcher = dockerHostPattern.matcher(host);
+        return matcher.matches() ? matcher.group(1) : host;
     }
 }
